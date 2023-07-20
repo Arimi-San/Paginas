c9629b2f79f475cbd86951435d13545deb7abc31
Arimi-San <135866378+Arimi-San@users.noreply.github.com>
2023-07-20 15:30:08 -0600

    Add files via upload

---
+++ Amenazas/Scrip.js
@@ -0,0 +1,168 @@
+
+var Encabezado = document.getElementById('contenido');
+var Parrafo = document.getElementById('contenido2');
+
+
+var CambioDeContenido = [
+    "¿Que Son?",
+    "¿Como Evitarlos?",
+    "Consejo 1: Utiliza contraseñas seguras",
+    "Consejo 2: Activa la autenticación de múltiples factores",
+    "Consejo 3: Mantén el software actualizado",
+    "Consejo 4: Cuida tus correos electrónicos y enlaces sospechosos",
+    "Consejo 5: Realiza copias de seguridad periódicas",
+
+];
+
+var CambioDeContenidoP = [
+    "Los niveles de seguridad informática se refieren a diferentes capas o niveles de protección que se implementan en un sistema o infraestructura para salvaguardar la integridad, confidencialidad y disponibilidad de la información y recursos frente a amenazas y vulnerabilidades. Estos niveles están diseñados para trabajar en conjunto y proporcionar una defensa en profundidad, lo que significa que, si un nivel se ve comprometido, otros niveles pueden seguir brindando protección",
+    "Para evitar las amenazas cibernéticas y proteger la seguridad de los sistemas, datos y usuarios, es fundamental implementar una serie de medidas de seguridad y buenas prácticas. ",
+    "Crea contraseñas únicas y robustas para cada una de tus cuentas en línea. Asegúrate de que sean lo suficientemente largas y contengan una combinación de letras (mayúsculas y minúsculas), números y caracteres especiales. Evita el uso de información personal obvia y nunca reutilices contraseñas en diferentes sitios.",
+    "Habilita la autenticación de múltiples factores siempre que sea posible. Esta medida añade una capa adicional de seguridad al requerir una segunda forma de autenticación, como un código enviado a tu teléfono móvil o una aplicación de autenticación, además de la contraseña.",
+    "Asegúrate de mantener todos tus dispositivos y aplicaciones actualizados con las últimas versiones y parches de seguridad. Las actualizaciones periódicas corrigen vulnerabilidades conocidas y ayudan a protegerte contra amenazas comunes.",
+    "Desconfía de correos electrónicos no solicitados o de remitentes desconocidos. No hagas clic en enlaces o descargues archivos adjuntos sospechosos. Los ciberdelincuentes a menudo utilizan técnicas de phishing para obtener información personal o infectar tu dispositivo con malware.",
+    "Respalda tus datos importantes de forma regular en un dispositivo externo o en la nube. Esto te permitirá restaurar tus archivos en caso de pérdida de datos debido a un ataque cibernético o a un fallo del sistema.",
+];
+
+function cambiarContenidoConAnimacion() {
+    var index = 0;
+    var totalCambios = CambioDeContenido.length;
+
+    function cambiarContenido() {
+        Encabezado.classList.add('fade-out');
+        Parrafo.classList.add('fade-out');
+        setTimeout(function () {
+            Encabezado.style.transform = 'translatex(-360%)';
+            Parrafo.style.transform = 'translatex(-360%)';
+            setTimeout(function () {
+                Encabezado.textContent = CambioDeContenido[index];
+                Parrafo.textContent = CambioDeContenidoP[index];
+                Encabezado.style.transform = 'translateY(0)';
+                Parrafo.style.transform = 'translateY(0)';
+                Encabezado.classList.remove('fade-out');
+                Parrafo.classList.remove('fade-out');
+                index++;
+                if (index === totalCambios) {
+                    reiniciarAnimacion();
+                } else {
+                    setTimeout(cambiarContenido, 1000);
+                }
+            }, 1000);
+        }, 1000);
+    }
+
+    cambiarContenido();
+}
+
+
+function reiniciarAnimacion() {
+    setTimeout(function () {
+        Encabezado.style.opacity = '1';
+        Parrafo.style.opacity = '1';
+        Encabezado.style.transform = 'translateY(2)';
+        Parrafo.style.transform = 'translateY(0)';
+        cambiarContenidoConAnimacion();
+    }, 5000);
+}
+
+cambiarContenidoConAnimacion();
+
+
+
+
+
+
+var Encabezado2 = document.getElementById('Seccion2Titulo');
+var Parrafo2 = document.getElementById('Seccion2Parrafo');
+
+
+var CambioDeContenido2 = [
+
+    "Amenazas Internas: Acceso no autorizado",
+    "Amenazas Internas: Negligencia",
+    "Amenazas Internas: Robo de datos",
+    "Amenazas Internas: Sabotaje",
+    "Amenazas Externas: Malware",
+    "Amenazas Externas: Ataques DDoS",
+    "Amenazas Externas: Phishing",
+    "Amenazas Externas: Ingeniería social",
+];
+
+
+var CambioDeContenidoP2 = [
+    "Cuando un empleado o persona con acceso privilegiado utiliza esta ventaja para acceder a datos o recursos sin permiso o para fines maliciosos.",
+    "Acciones no intencionales pero descuidadas por parte de empleados que pueden provocar vulnerabilidades o pérdida de datos.",
+    "La apropiación indebida de información confidencial por parte de empleados, ya sea para beneficio propio o para venderla a terceros.",
+    "Acciones deliberadas por parte de empleados descontentos o desleales para dañar la infraestructura, los datos o la reputación de una organización.",
+    "Software malicioso diseñado para dañar sistemas o robar información. Esto incluye virus, troyanos, ransomware y spyware.",
+    "Intentos de abrumar un sistema o red con tráfico excesivo para que no pueda responder a solicitudes legítimas.",
+    "Correos electrónicos o mensajes fraudulentos que intentan engañar a los usuarios para que revelen información personal o credenciales.",
+    "Manipulación psicológica de personas para obtener información confidencial o acceso no autorizado.",
+];
+
+
+function cambiarContenidoConAnimacion2() {
+    var index2 = 0;
+    var totalCambios2 = CambioDeContenido2.length;
+
+    function cambiarContenido2() {
+        Encabezado2.classList.add('fade-out');
+        Parrafo2.classList.add('fade-out');
+        setTimeout(function () {
+            Encabezado2.style.transform = 'translatex(-450%)';
+            Parrafo2.style.transform = 'translatex(260%)';
+            setTimeout(function () {
+                Encabezado2.textContent = CambioDeContenido2[index2];
+                Parrafo2.textContent = CambioDeContenidoP2[index2];
+                Encabezado2.style.transform = 'translateY(-0)';
+                Parrafo2.style.transform = 'translateY(0)';
+                Encabezado2.classList.remove('fade-out2');
+                Parrafo2.classList.remove('fade-out2');
+                index2++;
+                if (index2 === totalCambios2) {
+                    reiniciarAnimacion2();
+                } else {
+                    setTimeout(cambiarContenido2, 1000);
+                }
+            },);
+        }, 1000);
+    }
+
+    cambiarContenido2();
+}
+
+
+function reiniciarAnimacion2() {
+    setTimeout(function () {
+        Encabezado2.style.opacity = '1';
+        Parrafo2.style.opacity = '1';
+        Encabezado2.style.transform = 'translateY(0)';
+        Parrafo2.style.transform = 'translateY(0)';
+
+        cambiarContenidoConAnimacion2();
+    }, 5000);
+}
+
+
+cambiarContenidoConAnimacion2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Niveles/Script.js
+++ Niveles/Script.js
@@ -4,6 +4,7 @@ var Parrafo = document.getElementById('contenido2');
 
 
 var CambioDeContenido = [
+    "¿Que es",
     "Protección de datos",
     "Prevención de brechas de seguridad",
     "Cumplimiento normativo",
@@ -18,6 +19,7 @@ var CambioDeContenido = [
 ];
 
 var CambioDeContenidoP = [
+    "Los niveles de seguridad informática abarcan diferentes áreas de enfoque para proteger la información y los recursos de una organización. Estos niveles incluyen la seguridad física, de red, de sistemas operativos, de aplicaciones, de datos y la gestión de seguridad de la información. Cada nivel requiere estrategias y medidas específicas para mitigar riesgos y proteger los activos de la organización. Al aplicar estos niveles de seguridad de manera integral, se logra una protección efectiva en el entorno informático.",
     "Los niveles de seguridad ayudan a proteger los datos sensibles y confidenciales de una organización, evitando su acceso no autorizado, modificación o eliminación. Esto es especialmente crítico en el caso de información personal, financiera o estratégica, se refiere a la implementación de medidas y controles para garantizar la confidencialidad, integridad y disponibilidad de los datos de una organización. Incluye el uso de cifrado, políticas de acceso y permisos, copias de seguridad regulares, sistemas de detección de intrusos y prevención de pérdida de datos.",
     "Al tener diferentes niveles de seguridad en su lugar, las organizaciones pueden establecer barreras adicionales para prevenir y detectar posibles brechas de seguridad. Esto incluye el monitoreo de actividades sospechosas, la detección de intrusiones y la implementación de políticas de control de acceso,Consiste en la adopción de medidas para evitar que se produzcan brechas de seguridad, como ataques cibernéticos o acceso no autorizado a sistemas y datos. Implica la implementación de firewalls, sistemas de detección y prevención de intrusiones, monitoreo de actividad sospechosa, auditorías de seguridad y capacitación del personal en buenas prácticas de seguridad.",
     "Muchas industrias y jurisdicciones tienen regulaciones específicas sobre la seguridad de la información. La implementación de niveles de seguridad adecuados ayuda a garantizar el cumplimiento de estas normativas, evitando posibles sanciones legales y daños a la reputación de la organización,se refiere al cumplimiento de las regulaciones y estándares relacionados con la seguridad informática. Dependiendo del país y la industria, puede incluir normativas como la Ley de Protección de Datos, regulaciones específicas para sectores como salud o finanzas, y estándares de seguridad como ISO 27001. Cumplir con estas normas ayuda a garantizar la seguridad de los datos y proteger la organización de sanciones legales.",
@@ -29,13 +31,13 @@ var CambioDeContenidoP = [
 
 function cambiarContenidoConAnimacion() {
     var index = 0;
-    var totalCambios2 = CambioDeContenido.length;
+    var totalCambios = CambioDeContenido.length;
 
     function cambiarContenido() {
         Encabezado.classList.add('fade-out');
         Parrafo.classList.add('fade-out');
         setTimeout(function () {
-            Encabezado.style.transform = 'translatey(-450%)';
+            Encabezado.style.transform = 'translatey(-560%)';
             Parrafo.style.transform = 'translatey(260%)';
             setTimeout(function () {
                 Encabezado.textContent = CambioDeContenido[index];
@@ -45,13 +47,13 @@ function cambiarContenidoConAnimacion() {
                 Encabezado.classList.remove('fade-out');
                 Parrafo.classList.remove('fade-out');
                 index++;
-                if (index === totalCambios2) {
+                if (index === totalCambios) {
                     reiniciarAnimacion();
                 } else {
-                    setTimeout(cambiarContenido, 6000);
+                    setTimeout(cambiarContenido, 1000);
                 }
-            }, 2000);
-        }, 1000);
+            }, 1000);
+        }, 30000);
     }
 
     cambiarContenido();
@@ -108,13 +110,15 @@ var Parrafo2 = document.getElementById('contenido4');
 
 
 var CambioDeContenido2 = [
-    "Protección de datos",
-    "Prevención de brechas de seguridad",
-    "Cumplimiento normativo",
-    "Continuidad del negocio",
-    "Confianza del cliente",
-    "Mitigación de riesgos",
-    "Protección de la reputación",
+    "Nivel físico",
+    "Nivel de red",
+    "Nivel del sistema operativo",
+    "Nivel de aplicación",
+    "Nivel de datos",
+    "Nivel de identidad y acceso",
+    "Nivel de seguridad del software y codificación segura",
+    "Nivel de seguridad de la información y gestión de riesgos",
+    "Nivel de continuidad del negocio y recuperación de desastres",
 
 
 
@@ -122,13 +126,15 @@ var CambioDeContenido2 = [
 ];
 
 var CambioDeContenidoP2 = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
+    "Este nivel se enfoca en proteger los componentes físicos del sistema informático, como servidores, routers, conmutadores y otros dispositivos. Esto implica mantenerlos en lugares seguros, controlar el acceso físico y protegerlos contra daños, robos o manipulaciones",
+    "Se refiere a la seguridad de las comunicaciones y datos que fluyen a través de la red. Incluye medidas como el uso de firewalls, routers seguros, detección y prevención de intrusiones (IDS/IPS), VPNs (Virtual Private Networks) y encriptación para proteger los datos en tránsito.",
+    "Aquí se protege el sistema operativo del sistema informático. Esto implica mantener el sistema actualizado con los últimos parches de seguridad, deshabilitar servicios innecesarios y restringir el acceso a cuentas y recursos privilegiados.",
+    "La seguridad de las aplicaciones es fundamental para evitar vulnerabilidades y ataques a través de software. Desarrollar aplicaciones seguras, realizar pruebas de seguridad y parchear regularmente las vulnerabilidades son prácticas esenciales en este nivel.",
+    "La protección de datos es crucial en cualquier sistema informático. Esto implica implementar medidas de encriptación, establecer políticas de acceso y controlar el acceso a la información confidencial.",
+    "Este nivel se centra en la autenticación y autorización de usuarios. Utiliza contraseñas seguras, autenticación de múltiples factores (MFA), control de acceso basado en roles (RBAC) y otras técnicas para garantizar que solo las personas autorizadas tengan acceso a los recursos.",
+    "En este nivel, se fomenta el desarrollo de software seguro y se evita la inclusión de vulnerabilidades comunes como la inyección de código (SQL injection) y el Cross-Site Scripting (XSS).",
+    "Aquí se abordan las políticas, procedimientos y prácticas para proteger la información en general, incluyendo la clasificación de la información, la gestión de riesgos y la concienciación sobre seguridad.",
+    "Este nivel se ocupa de la planificación para mantener la continuidad del negocio en caso de interrupciones o desastres, y asegura la rápida recuperación y restauración de los servicios en caso de incidentes graves.",
 ];
 
 
@@ -151,24 +157,25 @@ function cambiarContenidoConAnimacion2() {
                 Parrafo2.classList.remove('fade-out2');
                 index2++;
                 if (index2 === totalCambios2) {
-                    reiniciarAnimacion();
+                    reiniciarAnimacion2();
                 } else {
-                    setTimeout(cambiarContenido2, 6000);
+                    setTimeout(cambiarContenido2, 1000);
                 }
-            }, 2000);
-        }, 1000);
+            }, );
+        }, 30000);
     }
 
     cambiarContenido2();
 }
 
 
-function reiniciarAnimacion() {
+function reiniciarAnimacion2() {
     setTimeout(function () {
         Encabezado2.style.opacity = '1';
         Parrafo2.style.opacity = '1';
         Encabezado2.style.transform = 'translateY(0)';
         Parrafo2.style.transform = 'translateY(0)';
+
         cambiarContenidoConAnimacion2();
     }, 5000);
 }
